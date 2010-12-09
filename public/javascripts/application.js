b66ef8070a499a864024af9dc41753b4df4fe8a7
Kazuya Takeshima <ktakeshima@accutechs.co.jp>
2010-12-09 16:42:59 +0900

    Add JavaScript actions and stylesheets.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,51 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+
+(function() {
+
+  const isDebug = true;
+
+  var log = function() {
+    if (isDebug && window.console) {
+      if (console.debug) {
+        return console.debug;
+      }
+      return console.log;
+    }
+    return Prototype.K;
+  }();
+
+  var getParentElementByAttributeRegExp = function(element, attribute, regexp) {
+    var parent_element = $(element);
+    do {
+      if (regexp.test(parent_element.readAttribute(attribute))) {
+        return parent_element;
+      }
+      parent_element = $(parent_element.parentNode);
+    } while (parent_element);
+    return null;
+  };
+
+  Object.extend(Element.prototype, {
+    getParentElementByAttributeRegExp: getParentElementByAttributeRegExp.methodize()
+  });
+
+  var showFullPost = function(post) {
+    post = $(post);
+    $$('#full').each(function(e) e.writeAttribute('id', null));
+    post.writeAttribute('id', 'full');
+  };
+
+  var onClickDocument = function(event) {
+    var target = $(event.target);
+    log(event, target);
+
+    var photo = target.getParentElementByAttributeRegExp('class', /thumbnail/);
+    if (photo) {
+      event.stop();
+      var post = photo.getParentElementByAttributeRegExp('class', /post/);
+      showFullPost(post);
+    };
+  };
+
+  document.observe('click', onClickDocument.bindAsEventListener(), false);
+
+})();
