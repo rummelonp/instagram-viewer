772402b3ecd1301dc4db0e194b9adf0fff321859
Kazuya Takeshima <mituki1412@gmail.com>
2010-12-11 15:09:08 +0900

    Bugfix javascript, stylesheets, and html.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,14 @@
 var showFullPost = function(post_id) {
-  var posts = document.getElementsByClassName('post');
-  for (var i = posts.length - 1; i >= 0; i -= 1) {
-    posts[i].id = '';
+  var post = document.getElementById('full');
+  if (post) {
+    post.id = '';
   }
-  var post = document.getElementsByClassName(post_id)[0];
-  post.id = 'full';
+  document.getElementsByClassName(post_id)[0].id = 'full';
 };
 
 window.onload = function() {
-  document.getElementsByClassName('post')[0].id = 'full';
+  var post = document.getElementById('full');
+  if (!post) {
+    document.getElementsByClassName('post')[0].id = 'full';
+  }
 };
