378e706d9f0dc536f0b6565c90623f88f64e9f34
Kazuya Takeshima <mituki1412@gmail.com>
2010-12-12 12:03:34 +0900

    Change cache time and bugfix comment user link.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  Instagram::Cached::setup RAILS_CACHE.cache_path, :expires_in => 5.minutes
+  Instagram::Cached::setup RAILS_CACHE.cache_path, :expires_in => 10.minutes
 
 end
