8c7934e8ddd14e48ec7216f242c301c33ad778a7
Kazuya Takeshima <mail@mitukiii.jp>
2010-12-20 00:11:45 +0900

    Write index controlle rspec tests.

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -2,24 +2,63 @@ require 'spec_helper'
 
 describe IndexController do
 
-  describe "GET 'index'" do
-    it "should be successful" do
-      get 'index'
-      response.should be_success
+  describe 'GET "popular"' do
+    before { get :popular }
+    describe :routes do
+      subject { {:get => '/popular'} }
+      it { should route_to(controller: 'index', action: 'popular') }
+    end
+    describe :response do
+      subject { response }
+      it { should be_success }
     end
   end
 
-  describe "GET 'user'" do
-    it "should be successful" do
-      get 'user'
-      response.should be_success
+  describe 'GET "popular.atom"' do
+    before { get :popular, format: :atom }
+    describe :routes do
+      subject { {:get => '/popular.atom'} }
+      it { should route_to(controller: 'index', action: 'popular', format: 'atom') }
+    end
+    describe :response do
+      subject { response }
+      it { should be_success }
     end
   end
 
-  describe "GET 'find'" do
-    it "should be successful" do
-      get 'find'
-      response.should be_success
+  describe 'GET "user/982876"' do
+    before { get :user, id: 982876 }
+    describe :routes do
+      subject { {:get => '/user/982876'} }
+      it { should route_to(controller: 'index', action: 'user', id: '982876') }
+    end
+    describe :response do
+      subject { response }
+      it { should be_success }
+    end
+  end
+
+  describe 'GET "user/982876.atom"' do
+    before { get :user, id: 982876, format: :atom }
+    describe :routes do
+      subject { {:get => '/user/982876.atom'} }
+      it { should route_to(controller: 'index', action: 'user', id: '982876', format: 'atom') }
+    end
+    describe :response do
+      subject { response }
+      it { should be_success }
+    end
+  end
+
+  describe 'POST "find"' do
+    before { post :find }
+    describe :routes do
+      subject { {:post => '/find'} }
+      it { should route_to(controller: 'index', action: 'find') }
+    end
+    describe :response do
+      subject { response }
+      it { should be_redirect }
     end
   end
 
