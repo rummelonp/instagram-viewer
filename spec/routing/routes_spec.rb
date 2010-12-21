8b9df6d45e47ceb1683147396be797eb5853f61c
Kazuya Takeshima <mail@mitukiii.jp>
2010-12-21 11:48:47 +0900

    Refactring routes spec.

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -2,27 +2,36 @@ require 'spec_helper'
 
 describe :routes do
 
-  describe :popular do
-    subject { {get: '/popular'} }
-    it { should route_to(controller: 'index', action: 'popular') }
+  describe 'GET "popular"' do
+    context :html do
+      subject { {get: '/popular'} }
+      it { should route_to(controller: 'index', action: 'popular') }
+    end
+    context :atom do
+      subject { {get: '/popular.atom'} }
+      it { should route_to(controller: 'index', action: 'popular', format: 'atom') }
+    end
   end
 
-  describe :popular, :atom do
-    subject { {get: '/popular.atom'} }
-    it { should route_to(controller: 'index', action: 'popular', format: 'atom') }
+  describe 'GET "user/982876"' do
+    context :html do
+      context 'without max id' do
+        subject { {get: '/user/982876'} }
+        it { should route_to(controller: 'index', action: 'user', id: '982876') }
+      end
+      context 'with max id "9578621"' do
+        subject { {get: '/user/982876/max_id/9578621'} }
+        it { should route_to(controller: 'index', action: 'user', id: '982876', max_id: '9578621') }
+      end
+    end
+
+    context :atom do
+      subject { {get: '/user/982876.atom'} }
+      it { should route_to(controller: 'index', action: 'user', id: '982876', format: 'atom') }
+    end
   end
 
-  describe :user do
-    subject { {get: '/user/982876'} }
-    it { should route_to(controller: 'index', action: 'user', id: '982876') }
-  end
-
-  describe :user, :atom do
-    subject { {get: '/user/982876.atom'} }
-    it { should route_to(controller: 'index', action: 'user', id: '982876', format: 'atom') }
-  end
-
-  describe :find do
+  describe 'POST "find"' do
     subject { {post: '/find', url: ''} }
     it { should route_to(controller: 'index', action: 'find') }
   end
