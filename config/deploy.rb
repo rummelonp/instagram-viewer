78e397f89a970db809661f3cd214614d12563ed3
Kazuya Takeshima <mail@mitukiii.jp>
2010-12-22 09:12:45 +0900

    Set capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,15 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "instagram"
+set :repository,  "git://github.com/mitukiii/instagram-viewer.git"
+set :scm, :git
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_to, "/var/www/html/mitukiii/#{application}"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "mitukiii.jp"
+role :app, "mitukiii.jp"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  # Restart the application servers.
+  task :restart, :roles => :app do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
